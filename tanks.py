a83694ef5448fb94187b62ab71d63e17d39ede0f
g-vinokurov <g.vinokurov@g.nsu.ru>
2025-04-27 10:16:12 +0700

    Add music, fix bug of tank and walls collision, split world on walls, ground and waters, add shots

--- tanks.py
+++ tanks.py
@@ -116,17 +116,18 @@ class Tank(pygame.sprite.Sprite):
             self.original_image, 
             self.direction.value
         )
+        # Обновляем прямоугольник (он у нас повернулся)
+        rect = self.image.get_rect()
+        rect.centerx = self.rect.centerx
+        rect.centery = self.rect.centery
+        self.rect = rect
         # Обновляем маску (т.к. поменялось положение пикселей)
         self.mask = pygame.mask.from_surface(self.image)
 
 
 class PlayerTank(Tank):
     def update(self):
-        global world
         global x_shift, y_shift
-
-        WORLD_H = world_height * TILE_SIZE
-        WORLD_W = world_width * TILE_SIZE
 
         if not self.on_move:
             return
@@ -163,7 +164,7 @@ class PlayerTank(Tank):
         self.rect.centerx = round(self.x)
         self.rect.centery = round(self.y)
 
-        if spritecollideany(self, world, collide_mask):
+        if spritecollideany(self, walls, collide_mask):
             self.x = old_x
             self.y = old_y
             x_shift = old_x_shift
@@ -194,27 +195,56 @@ class EnemyTank(Tank):
         self.rect.centery = round(self.y - y_shift)
 
 
-tank = PlayerTank('tank-1.png', 3, 3)
-enemy = EnemyTank('tank-2-enemy.png', 21, 21)
+class Fire(pygame.sprite.Sprite):
+    def __init__(self, image, x, y, direction):
+        super().__init__()
+        self.image = pygame.image.load(f'tiles/{image}')
+        self.w = TILE_SIZE
+        self.h = TILE_SIZE
+        self.size = (self.w, self.h)
 
-world_height = 25
-world_width = 25
+        self.direction = direction  
+        
+        self.original_image = pygame.transform.scale(
+            self.image, self.size
+        )
+        self.image = pygame.transform.scale(
+            self.image, self.size
+        )
+        self.image = pygame.transform.rotate(
+            self.image, self.direction.value
+        )
+        self.mask = pygame.mask.from_surface(self.image)
+        
+        self.rect = self.image.get_rect()
+        self.x = x
+        self.y = y
 
-ground = pygame.sprite.Group()
+        self.rect.centerx = round(self.x)
+        self.rect.centery = round(self.y)
 
-for i in range(world_height):
-    for j in range(world_width):
-        tile = Tile(default_tile, i, j)
-        ground.add(tile)
+        self.speed = TILE_SIZE / 6 * 2
+    
+    def update(self):
+        if self.direction == Direction.Up:
+            self.y -= self.speed
+        if self.direction == Direction.Down:
+            self.y += self.speed
+        if self.direction == Direction.Left:
+            self.x -= self.speed
+        if self.direction == Direction.Right:
+            self.x += self.speed
+        
+        self.rect.centerx = round(self.x)
+        self.rect.centery = round(self.y)
 
-world = pygame.sprite.Group()
-
-# world = []
-# for i in range(world_height):
-#     row = []
-#     for j in range(world_width):
-#         row.append(None)
-#     world.append(row)
+        if self.x < 0 or self.y < 0 or self.x > WORLD_W or self.y > WORLD_H:
+            # Удаляет спрайт огня из всех групп (группа fires)
+            # Удаляет тогда, когда тайл вышел за границы поля
+            self.kill()
+        
+        if spritecollideany(self, walls, collide_mask):
+            self.kill()
 
 x_shift = 0
 y_shift = 0
@@ -232,7 +262,9 @@ ARROWS = [
 
 
 def load_world():
-    global world, world_height, world_width
+    global world_height, world_width
+    global WORLD_H, WORLD_W
+    global walls, waters
 
     file = open('map.txt', 'r')
 
@@ -240,14 +272,12 @@ def load_world():
     world_size = world_info.strip().split()
     world_width = int(world_size[0])
     world_height = int(world_size[1])
+
+    WORLD_H = world_height * TILE_SIZE
+    WORLD_W = world_width * TILE_SIZE
     
-    world = pygame.sprite.Group()
-    # world = []
-    # for i in range(world_height):
-    #     row = []
-    #     for j in range(world_width):
-    #         row.append(None)
-    #     world.append(row)
+    waters = pygame.sprite.Group()
+    walls = pygame.sprite.Group()
 
     for line in file:
         line = line.strip()
@@ -257,13 +287,43 @@ def load_world():
         j = int(j)
         image = name_to_tile[tile]
         
-        world.add(Tile(image, i, j))
+        tile = Tile(image, i, j)
+        if image == water:
+            waters.add(tile)
+        if image == stone:
+            walls.add(tile)
         
     file.close()
 
 
 load_world()
 
+ground = pygame.sprite.Group()
+
+for i in range(world_height):
+    for j in range(world_width):
+        tile = Tile(default_tile, i, j)
+        ground.add(tile)
+
+tank = PlayerTank('tank-1.png', 3, 3)
+enemy = EnemyTank('tank-2-enemy.png', 21, 21)
+
+fires = pygame.sprite.Group()
+
+
+sound_battle = pygame.mixer.Sound('audio/battle.mp3')
+sound_shot = pygame.mixer.Sound('audio/shot.mp3')
+sound_tank = pygame.mixer.Sound('audio/tank_move.mp3')
+
+channel_0 = pygame.mixer.Channel(0)
+channel_1 = pygame.mixer.Channel(1)
+channel_2 = pygame.mixer.Channel(2)
+
+channel_0.set_volume(0.2)  # У разных каналов своя громкость (от 0 до 1)
+channel_1.set_volume(0.8)  # Чем меньше значение, тем меньше громкость
+channel_2.set_volume(0.5)
+
+channel_0.play(sound_battle, -1)  # -1 для бесконечного проигрывания
 
 run = True
 while run:
@@ -280,38 +340,39 @@ while run:
                 tank.change_direction(Direction.Left)
             if event.key == pygame.K_RIGHT:
                 tank.change_direction(Direction.Right)
+        
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+            fire = Fire('fire.png', tank.x, tank.y, tank.direction)
+            fires.add(fire)
+            channel_1.play(sound_shot, 0)
             
         if event.type == pygame.KEYDOWN:
             if event.key in ARROWS:
                 tank.on_move = True
+                channel_2.play(sound_tank, -1)
         
         if event.type == pygame.KEYUP:
             if event.key in ARROWS:
                 tank.on_move = False
+                channel_2.stop()
     
     ground.update()
-    world.update()
+    waters.update()
+    walls.update()
     tank.update()
     enemy.update()
-    
+    fires.update()
+
     screen.fill(BLACK)
-    
-    # for i in range(world_height):
-    #     for j in range(world_width):
-    #         x = j * TILE_SIZE - x_shift
-    #         y = i * TILE_SIZE - y_shift
-    #         
-    #         screen.blit(default_tile, (x, y))
-    # 
-    #         cell = world[i][j]
-    #         if cell is not None:
-    #             screen.blit(cell, (x, y))
 
     ground.draw(screen)
-    world.draw(screen)
-
+    waters.draw(screen)
+    walls.draw(screen)
+    
     screen.blit(tank.image, tank.rect)
     screen.blit(enemy.image, enemy.rect)
+
+    fires.draw(screen)
     
     pygame.time.delay(25)
     pygame.display.update()
